d0b69cf2bd66cff1a85f90e8a407b654ecf475fa
Chen Yi <Master_YiChen@163.com>
2021-01-16 19:20:41 +0800

    更新分屏控件样式

--- src/js/component/splitIcon.jsx
+++ src/js/component/splitIcon.jsx
@@ -5,6 +5,7 @@ import EventBus from '../eventBus'
 class SplitIcon extends React.PureComponent {
 
     state = {
+        transformLine: 'transform(-1px,0)',
         transform: 'translate(-20px, -20px)'
     }
 
@@ -46,7 +47,8 @@ class SplitIcon extends React.PureComponent {
             let px = x - window.innerWidth / 2
             px = Math.min(Math.max(px, -window.innerWidth / 2), window.innerWidth / 2);
             this.setState({
-                transform: 'translate(' + (px - 20) + 'px, -20px)'
+                transform: 'translate(' + (px - 20) + 'px, -20px)',
+                transformLine: 'translate(' + (px - 1) + 'px, 0px)'
             })
 
             EventBus.emit(EventBus.Event.splicIcon_event, px);
@@ -63,19 +65,35 @@ class SplitIcon extends React.PureComponent {
 
 
     render() {
-        const style = {
-            position: 'absolute',
-            cursor: 'ew-resize',
-            width: '40px',
-            height: '40px',
-            backgroundColor: '#F32196',
-            opacity: 0.7,
-            borderRadius: '50%',
-            top: '50%',
-            left: '50%',
-            transform: this.state.transform
+
+        const styles = {
+            line: {
+                position: 'absolute',
+                width: '2px',
+                height: '100%',
+                backgroundColor: 'blue',
+                opacity: 0.5,
+                left: '50%',
+                transform: this.state.transformLine
+            },
+            thumb: {
+                position: 'absolute',
+                cursor: 'ew-resize',
+                width: '40px',
+                height: '40px',
+                backgroundColor: '#F32196',
+                opacity: 0.5,
+                borderRadius: '50%',
+                top: '50%',
+                left: '50%',
+                transform: this.state.transform
+            }
         }
-        return (<div ref={(dom) => this.dom = dom} style={style}></div>)
+        return (
+            <div>
+                <div style={styles.line}></div>
+                <div ref={(dom) => this.dom = dom} style={styles.thumb}></div>
+            </div>)
     }
 
     _ResizeSet() {
